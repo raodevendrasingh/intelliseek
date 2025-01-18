5dd043c8e7fc59c31bfa00fd4cf8c29cbdce0b23
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2025-01-18 23:02:45 +0530

    feat: file formatting via biome

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
-// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
-// (when running the application with `next dev`), for more information see:
-// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
-
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
+
+if (process.env.NODE_ENV === "development") {
+    (async () => {
+        await setupDevPlatform();
+    })();
+}
 
 export default nextConfig;
