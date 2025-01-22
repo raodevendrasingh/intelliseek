79e6dc8b19fd1e1be96775ae44f47f0c72f73414
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2025-01-22 21:22:53 +0530

    feat: add badge and tooltip to textbox and update schema

--- src/app/(core)/chat/page.tsx
+++ src/app/(core)/chat/page.tsx
@@ -6,97 +6,132 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
 } from "@/components/ui/form";
 import { z } from "zod";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
-import { Paperclip, ArrowRight, SpinnerBall } from "@phosphor-icons/react";
+import { ArrowRight, SpinnerBall, GlobeSimple } from "@phosphor-icons/react";
 import { Button } from "@/components/ui/button";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Badge } from "@/components/ui/badge";
+import { Paperclip } from "lucide-react";
 
 export default function ChatPage() {
-    const [isLoading, setLoading] = useState<boolean>(false);
-    const router = useRouter();
-    const form = useForm<z.infer<typeof querySchema>>({
-        resolver: zodResolver(querySchema),
-        defaultValues: {
-            query: "",
-        },
-    });
+	const [isLoading, setLoading] = useState<boolean>(false);
+	const router = useRouter();
+	const form = useForm<z.infer<typeof querySchema>>({
+		resolver: zodResolver(querySchema),
+		defaultValues: {
+			query: "",
+		},
+	});
 
-    const onSubmit = async (queryData: z.infer<typeof querySchema>) => {
-        setLoading(true);
-        console.log(queryData);
-        try {
-        } catch (error) {
-            console.log(error);
-            alert("Error submitting query");
-        }
-        setLoading(false);
-    };
-    return (
-        <div className="w-full mx-auto min-h-screen flex justify-center items-center px-5">
-            <div className="flex flex-col justify-center items-center w-full mx-auto">
-                <div className="text-center pb-10">
-                    <h1 className="text-3xl font-bold">
-                        What can I help you with?
-                    </h1>
-                </div>
-                <Card className="min-w-max max-w-2xl w-full p-2 space-y-2 bg-accent">
-                    <CardContent className="w-full p-0">
-                        <Form {...form}>
-                            <form>
-                                <FormField
-                                    control={form.control}
-                                    name="query"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormControl>
-                                                <Textarea
-                                                    placeholder="Add a resource and start chatting..."
-                                                    className="resize-none w-full border-0"
-                                                    rows={3}
-                                                    {...field}
-                                                />
-                                            </FormControl>
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                            </form>
-                        </Form>
-                    </CardContent>
-                    <CardFooter className="p-0">
-                        <div className="flex justify-between items-center w-full">
-                            <Button
-                                variant="outline"
-                                className="rounded-full h-8"
-                            >
-                                <Paperclip size={32} />
-                                <span>Context</span>
-                            </Button>
-                            <Button
-                                size="icon"
-                                className="h-8 w-8 rounded-full"
-                                onClick={form.handleSubmit(onSubmit)}
-                            >
-                                {isLoading ? (
-                                    <SpinnerBall
-                                        size={32}
-                                        className="animate-spin"
-                                    />
-                                ) : (
-                                    <ArrowRight size={32} />
-                                )}
-                            </Button>
-                        </div>
-                    </CardFooter>
-                </Card>
-            </div>
-        </div>
-    );
+	const isQueryEmpty = form.watch("query") === "";
+
+	const onSubmit = async (queryData: z.infer<typeof querySchema>) => {
+		setLoading(true);
+		console.log(queryData);
+		try {
+			const response = await fetch("/api/chat", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(queryData),
+			});
+
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			console.log(error);
+			alert("Error submitting query");
+		}
+		setLoading(false);
+	};
+	return (
+		<div className="w-full mx-auto min-h-screen flex justify-center items-center px-5">
+			<div className="flex flex-col justify-center items-center w-full mx-auto">
+				<div className="text-center pb-10">
+					<h1 className="text-3xl font-bold">
+						What can I help you with?
+					</h1>
+				</div>
+				<Card className="min-w-max max-w-2xl w-full p-3 space-y-3 bg-accent rounded-3xl">
+					<CardContent className="w-full p-0">
+						<Form {...form}>
+							<form>
+								<FormField
+									control={form.control}
+									name="query"
+									render={({ field }) => (
+										<FormItem>
+											<FormControl>
+												<Textarea
+													placeholder="Add a resource and start chatting..."
+													className="resize-none w-full border-0"
+													rows={2}
+													{...field}
+												/>
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</form>
+						</Form>
+					</CardContent>
+					<CardFooter className="p-0">
+						<div className="flex justify-between items-center w-full">
+							<Badge
+								variant="outline"
+								className="bg-primary-foreground space-x-1 rounded-full text-primary/70 hover:text-emerald-600 hover:border-emerald-600 transition-colors cursor-pointer px-2 py-1"
+							>
+								<GlobeSimple size={20} />
+								<span>Web</span>
+							</Badge>
+
+							<div className="flex items-center gap-3">
+								<TooltipProvider>
+									<Tooltip>
+										<TooltipTrigger className="flex items-center gap-1 rounded-full h-8 px-2 hover:text-emerald-600 hover:bg-primary/10 bg-transparent transition-colors text-primary/70">
+											<Paperclip size={18} />
+										</TooltipTrigger>
+										<TooltipContent>
+											Attach file and images for context
+										</TooltipContent>
+									</Tooltip>
+								</TooltipProvider>
+
+								<Button
+									size="icon"
+									className="h-8 w-8 rounded-full bg-emerald-500 hover:bg-emerald-700 cursor-pointer"
+									onClick={form.handleSubmit(onSubmit)}
+									disabled={isQueryEmpty}
+								>
+									{isLoading ? (
+										<SpinnerBall
+											size={32}
+											className="animate-spin"
+										/>
+									) : (
+										<ArrowRight size={32} />
+									)}
+								</Button>
+							</div>
+						</div>
+					</CardFooter>
+				</Card>
+			</div>
+		</div>
+	);
 }
--- src/lib/app-schema.ts
+++ src/lib/app-schema.ts
@@ -1,12 +1,10 @@
 import { z } from "zod";
 
 export const querySchema = z.object({
-    textContext: z
-        .string()
-        .max(1000, { message: "Name must be less than 1024 characters" })
-        .optional(),
-    query: z
-        .string()
-        .max(1000, { message: "Name must be less than 512 characters" })
-        .optional(),
+	query: z
+		.string()
+		.max(1024, { message: "Name must be less than 1024 characters" })
+		.optional(),
+	file: z.any().optional(),
+	type: z.enum(["image", "text", "weblink", "file"]).default("text"),
 });
