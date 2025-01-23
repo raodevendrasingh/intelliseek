584b7fa91863226c99c1e0805208e49218cb2b1d
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2025-01-23 23:03:07 +0530

    fix params error

--- src/app/(core)/chat/[chat-id]/page.tsx
+++ src/app/(core)/chat/[chat-id]/page.tsx
@@ -2,12 +2,18 @@
 
 import { QueryBox } from "@/components/query-box";
 
-export default function ChatPage({ params }: { params: { chatId: string } }) {
-    return (
-        <div className="w-full mx-auto min-h-screen flex">
-            <div className="absolute bottom-10 flex flex-col w-full mx-auto">
-                <QueryBox windowType="chat" chatId={params.chatId} />
-            </div>
-        </div>
-    );
+interface PageProps {
+	params: {
+		"chat-id": string;
+	};
+}
+
+export default function ChatPage({ params }: PageProps) {
+	return (
+		<div className="w-full mx-auto min-h-screen flex">
+			<div className="absolute bottom-10 flex flex-col w-full mx-auto">
+				<QueryBox windowType="chat" chatId={params["chat-id"]} />
+			</div>
+		</div>
+	);
 }
