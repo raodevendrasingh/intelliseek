a709f33e2b52f9367e0d2821307ff5889a69f104
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2025-01-27 20:05:27 +0530

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,15 @@ import { Outfit } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
-const outfit = Outfit({
-    variable: "--font-outfit",
-    subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
     title: "Intelliseek",
     description: "AI Based Information Retrival from the Provided Context.",
 };
+
+const outfit = Outfit({
+    variable: "--font-outfit",
+    subsets: ["latin"],
+});
 
 export default function RootLayout({
     children,
@@ -20,6 +20,26 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="dark" suppressHydrationWarning>
+            <head>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
+            </head>
             <body className={`${outfit.className} antialiased`}>
                 {children}
                 <Toaster />
